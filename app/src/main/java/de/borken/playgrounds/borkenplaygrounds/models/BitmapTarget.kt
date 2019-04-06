28d91799edc74c668cf47e4a7280a744cd770cb2
Thomas Buning <thomas.buning@gmail.com>
2019-04-06 07:04:51 +0200

    Fix Rotation Bug

--- app/src/main/java/de/borken/playgrounds/borkenplaygrounds/models/BitmapTarget.kt
+++ app/src/main/java/de/borken/playgrounds/borkenplaygrounds/models/BitmapTarget.kt
@@ -8,11 +8,10 @@ import com.bumptech.glide.request.transition.Transition
 open class BitmapTarget: CustomTarget<Bitmap>() {
 
     override fun onLoadCleared(placeholder: Drawable?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
 }
